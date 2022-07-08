ffe4227720bbd11ddcfc22c751ceebb8583b3e14
Boussaid Hocine <boussaid.ho@gmail.com>
2022-07-08 09:23:45 +0200

    ajout panier

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\AdminType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -33,4 +36,56 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/passer-en-admin_{id<\d+>}", name="passer_en_admin")
+     */
+    public function passerEnAdmin( $id, UserRepository $repo, Request $request)
+    {
+        $secret = "123456";
+
+        $form = $this->createForm(AdminType::class);
+        $form->handleRequest($request);
+
+        // on récupere l'utilisateur dont l'id est celui passé dan l'url
+        $user = $repo->find($id);
+
+        if (!$user) {
+            $this->addFlash("error", "aucun utilisateur trouvé avec l'id $id");
+            return $this->redirectToRoute("app_home");
+        }
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+  
+            //si la sisie dans le champ "secret" du formulaire correspond au mdp stocké dans la variable $secret
+            if ($form->get('secret')->getData() == $secret) {
+
+                $user->setRoles(["ROLE_ADMIN"]);
+
+            }else{
+                $this->addFlash("error", "vous n'avez pas les droits pour affectuer cette action, veuillez contacter l'administrateur du site !");
+                return $this->redirectToRoute("app_home");
+            }
+
+            // en passant par la methode add du repository, l'objet sera parsisté et envoyé en bdd grâce au parametre 1 (true) 
+            $repo->add($user, 1);
+
+            $this->addFlash("success", "Vous êtes désormais Admin, veuillez vous reconnecter pour profiter de vos priviéges");
+            return $this->redirectToRoute("app_home");
+
+        }
+
+        return $this->render("security/passerEnAdmin.html.twig", [
+            "user" => $user,
+            "formAdmin" => $form->createView()
+        ]);
+
+
+    }
+
+
+
+
+
 }
